09ee2203ddb7eb9b3f26ef21d86d3e0ecabbabbb
Crystian Peralta <134974034+Crystian9513@users.noreply.github.com>
2023-05-30 18:50:25 -0500

    Pro-Salud

---
+++ js/sesion.js
@@ -0,0 +1,33 @@
+function validarUsuario(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    //  verificar si el usuario y contraseña son correctos.
+    if (username === 'crystian' && password === '1234') {
+      window.location.href = "../sesion.html";
+    } else {
+      alert('Usuario o contraseña incorrectos.');
+    }
+  };
+
+  // desplazamiento de imagenes y formularios.
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
+  
+
+ 
